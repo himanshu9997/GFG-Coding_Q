2eeda8ba19e988d4de3bd7fefa221b8cc0eaf071
Himanshu Chaurasiya <137139619+himanshu9997@users.noreply.github.com>
2024-10-06 19:05:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // consume the newline
+        while (t-- > 0) {
+            int k = sc.nextInt();
+            sc.nextLine(); // consume the newline
+            String input = sc.nextLine();
+            String[] strNumbers = input.split(" ");
+            int[] arr = new int[strNumbers.length];
+            for (int i = 0; i < strNumbers.length; i++) {
+                arr[i] = Integer.parseInt(strNumbers[i]);
+            }
+            Solution ob = new Solution();
+            int res = ob.binarysearch(arr, k);
+            System.out.println(res);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        int low = 0;                
+        int high = arr.length - 1;   
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            
+            if (arr[mid] == k) {
+                return mid;  
+            } else if (arr[mid] < k) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1;  
+            }
+        }
+        
+        
+        return -1;
+    }
+}
